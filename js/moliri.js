1e6226fa2dee5bb6cab338e38f071425347de8f9
moliri <lmaliakal@u.northwestern.edu>
2015-08-26 15:59:38 -0500

    basic outline of personal site

--- js/moliri.js
+++ js/moliri.js
@@ -1,25 +1,7 @@
 "use strict";
 
-/* Global variables and objects */
-
-// Handling browser navigation
-$(window).on("navigate", function (event, data) {
-  var direction = data.state.direction;
-  
-  if (direction === 'back') {    
-    if($.mobile.activePage.is('#recipeItem')){
-        $('#recipes .recipeList').empty();
-        populateRecipeList();
-    }
-  }
-  
-});
-
-///////////////////////////////////////
-/* start page events and functions */
-
-/* starting script for intro page */
-$(document).on('pageinit', '#intropage', function(){
+$(document).ready(function(){
+  $('.bxslider').bxSlider();
 });
 
 
